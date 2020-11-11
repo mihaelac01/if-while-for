f3a46cb777368c94334b6525d5a13d37fdc2bfd9
Mihaela Cocieru <70962062+mihaelac01@users.noreply.github.com>
2020-11-11 12:18:48 +0300

    Add files via upload

---
+++ problema 1.py
@@ -0,0 +1,7 @@
+n=int(input('n='))
+if n==28 or n==29:
+    print('februarie') 
+if n==30:
+    print("aprilie","iunie","septembrie","noiembrie")
+if n==31:
+    print("ianuarie","martie","mai","iulie","august","octombrie","decembrie")
